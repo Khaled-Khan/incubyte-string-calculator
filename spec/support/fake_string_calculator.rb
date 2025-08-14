b07c5e0fb8152924f35d7010d060db5179ae8ea8
Khaled Khan <khaledkhan7744@gmail.com>
2025-08-14 16:13:45 +0530

    support for new lines as delimiters

--- spec/support/fake_string_calculator.rb
+++ spec/support/fake_string_calculator.rb
@@ -1,7 +1,7 @@
 class StringCalculator
     def self.add(numbers)
-      return 0 if numbers == ""
-      nums = numbers.split(",").map(&:to_i)
-      nums.sum
+      return 0 if numbers.empty?
+      numbers.split(/,|\n/).map(&:to_i).sum
     end
   end
+
